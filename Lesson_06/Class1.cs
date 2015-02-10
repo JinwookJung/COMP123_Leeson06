9cd737f0a3f0ea66c7f9c65676f2444052529b03
Jinwook Jung <dddrdf@gmail.com>
2015-02-10 13:10:28 -0500

    Added get set

---
+++ Lesson_06/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_06
+{
+    class Class1
+    {
+        //PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++
+        //just hold the data
+        private int integerNumber;
+        private string stringVariable;
+        private double doubleNumber = 50.0d;
+
+        //PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++
+        //access to the data
+        public int IntegerNumber
+        {
+            get 
+            {
+                return this.integerNumber; //RETURNS VALUE OF INSTANCE
+            }
+            set 
+            {
+                Console.WriteLine("");
+                this.integerNumber = value * 2; //SET OUR INTANCE VARIABLE
+            }
+        }
+
+        public string StringVariable
+        {
+            get 
+            {
+                return this.stringVariable; //READ ONLY PROPERTY
+            }
+        }
+
+        public double DoubleNumber{ get; set; }
+
+        public Class1(string theString)
+        {
+            this.stringVariable = theString;
+        }
+
+        public void show()
+        {
+            
+            Console.Write("++++++++++++++++++++++");
+            Console.Write("\n");
+            Console.WriteLine("IntegerNumber: {0}", this.integerNumber);
+            Console.WriteLine("stringVariable: {0}", this.stringVariable);
+            Console.WriteLine("DoubleNumber: {0}", this.doubleNumber);
+            Console.Write("++++++++++++++++++++++");
+
+        }
+    }
+}
---
+++ Lesson_06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myClass = new Class1("Hello");
+
+            myClass.IntegerNumber = 10;
+            //myClass.StringVariable = "Hi";
+
+
+            
+            //Console.WriteLine(myClass);
+
+            myClass.show();
+
+            Console.Write("/n");
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+            Console.WriteLine("Please hit any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
